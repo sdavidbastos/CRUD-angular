da9c63a4b9ae3130c25334b52951546ee004fa27
sdavidbastos <davidtavares98@gmail.com>
2021-01-26 16:06:14 -0300

    Component Read Concluded

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../produc.model';
-import { ProductService } from '../product.service';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../produc.model";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-read',
-  templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.css']
+  selector: "app-product-read",
+  templateUrl: "./product-read.component.html",
+  styleUrls: ["./product-read.component.css"],
 })
 export class ProductReadComponent implements OnInit {
+  products: Product[] = [];
 
-  products: Product[]
-
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.read().subscribe(products => {
-      this.products = products
-      console.log(products)
-    })
+    this.productService.read().subscribe((products) => {
+      this.products = products;
+      console.log(products);
+    });
   }
-
 }
