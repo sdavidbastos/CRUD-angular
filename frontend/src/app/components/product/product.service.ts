f517b4085dacdb120dee3ef663dc27dc17d0793f
sdavidbastos <davidtavares98@gmail.com>
2021-02-02 09:23:11 -0300

    Add readById and update in product.service

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,28 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar'
-import { Observable } from 'rxjs';
-import { Product } from './produc.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
+import { Product } from "./produc.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
-  baseUrl = "http://localhost:3001/products"
-  constructor(private snackBar: MatSnackBar, private http:HttpClient) { }
+  baseUrl = "http://localhost:3001/products";
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg:string): void{
-    this.snackBar.open(msg, 'X', {
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, "X", {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      verticalPosition: "top",
+    });
   }
 
-  create(product: Product):Observable<Product>{
-    return this.http.post<Product>(this.baseUrl, product)
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
   }
-  read():Observable<Product[]>{
-    return this.http.get<Product[]>(this.baseUrl)
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
+  update(product:Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product)
   }
 }
