ff4f5548027496525293c6aca18fffc5e69bdf75
sdavidbastos <davidtavares98@gmail.com>
2021-02-03 19:02:18 -0300

    Projeto Concluido

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 
 import { Router } from "@angular/router";
+import { HeaderService } from "src/app/components/template/header/header.service";
 @Component({
   selector: "app-product-crud",
   templateUrl: "./product-crud.component.html",
   styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastro de Produtos",
+      icon: "storefront",
+      routerUrl: "/products",
+    };
+  }
 
   ngOnInit(): void {}
 
